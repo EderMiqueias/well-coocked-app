7d86a7276acb400f5e2b7c541ad6addc954e9d2c
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2023-03-09 23:14:29 -0300

    add Sobre Page

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import '@/assets/styles/App.css';
+import { BackIconComponent } from '@/components';
+import { Content, SobreContainer, Title } from './styles';
 
 const SobrePage = () => {
-  return (<p>asasas</p>);
-}
+  return (
+  <SobreContainer>
+    <BackIconComponent backTo="/" />
+    <Title>Well Coocked!</Title>
+    <Content>
+      Well Coocked é um jogo de temática culinária com dinamica baseada
+      na contrução de um algoritmo para preparação de um prato solicitado.
+    </Content>
+    <Content>
+      Ajude o Cheff Droid a conquistar seu lugar como o chefe mais eficiente
+      que uma cozinha de restaurante pode ter.
+    </Content>
+  </SobreContainer>);
+};
 
 export { SobrePage };
---
+++ src/pages/Sobre/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { MenuContainer } from '../Home/styles';
+
+export const SobreContainer = styled(MenuContainer)`
+  color: white;
+  max-width: 600px;
+`;
+
+export const Title = styled.h1``;
+
+export const Subtitle = styled.h3``;
+
+export const Content = styled.p``;
+
