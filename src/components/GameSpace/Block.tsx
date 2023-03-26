b8468246752ebf0770fc343d66110daee6f7a148
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2023-03-26 16:23:28 -0300

    add constants

--- src/components/GameSpace/Block.tsx
+++ src/components/GameSpace/Block.tsx
@@ -4,6 +4,7 @@ import { BlockState, Coords, MobileItems } from "@/types";
 
 import { BlockContainer, ItemsContainer } from "./styles";
 import { MobileItem, ImmobileItem } from "../Items";
+import { BLOCK_HEIGHT, BLOCK_WIDTH } from "@/constants";
 
 type BlockProps = {
   coords: Coords;
@@ -16,8 +17,8 @@ type BlockProps = {
 export const Block: React.FC<BlockProps> = ({
   coords,
   state,
-  width = 216,
-  height = 128,
+  width = BLOCK_WIDTH,
+  height = BLOCK_HEIGHT,
   colorFase = false
 }) => {
   const isPan = state.mobileItem === MobileItems.pan;
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const BLOCK_WIDTH = 216;
+export const BLOCK_HEIGHT = 128;
