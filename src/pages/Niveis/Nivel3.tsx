7d28342b8bf8eb8cc8330fa78a8f00847d954ea3
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2023-04-11 00:31:06 -0300

    update nivel 3

--- src/pages/Niveis/Nivel3.tsx
+++ src/pages/Niveis/Nivel3.tsx
@@ -4,19 +4,19 @@ import { BlockState, Coords, Dishs, GameSpaceState, ImmobileItems, MobileItems }
 import { NivelBase } from "./NivelBase";
 
 export const Nivel3 = () => {
-  const initialCoords = { y: 3, x: 4 } as Coords;
-  const dish = Dishs.baiao;
+  const initialCoords = { y: 1, x: 3 } as Coords;
+  const dish = Dishs.feijoada;
 
   const getInitialState = (): GameSpaceState => {
-    let state = getNivelInitialState(4, 4, dish, 50);
+    let state = getNivelInitialState(4, 4, dish, 30);
 
     updateBlock(state, initialCoords.y, initialCoords.x, {
       isMainCharacter: true
     } as BlockState);
-    updateBlock(state, 2, 2, {
-      mobileItem: getMobileIconState(MobileItems.potato)
-    } as BlockState);
     updateBlock(state, 2, 3, {
+      mobileItem: getMobileIconState(MobileItems.bean)
+    } as BlockState);
+    updateBlock(state, 3, 2, {
       mobileItem: getMobileIconState(MobileItems.pan),
       immobileItem: {
         item: ImmobileItems.stove
@@ -24,6 +24,20 @@ export const Nivel3 = () => {
     } as BlockState);
     updateBlock(state, 2, 4, {
       mobileItem: getMobileIconState(MobileItems.dish)
+    } as BlockState);
+    updateBlock(state, 3, 1, {
+      mobileItem: getMobileIconState(MobileItems.dish)
+    } as BlockState);
+    updateBlock(state, 3, 4, {
+      mobileItem: getMobileIconState(MobileItems.pan)
+    } as BlockState);
+    updateBlock(state, 4, 3, {
+      mobileItem: getMobileIconState(MobileItems.dish)
+    } as BlockState);
+    updateBlock(state, 4, 4, {
+      immobileItem: {
+        item: ImmobileItems.stove
+      }
     } as BlockState);
 
     return state;
